a1e22b0878b21b4055b939b23781b371dafb8146
Thomas <thomas@lekoala.be>
2022-06-09 11:36:12 +0200

    add callbacks

--- client/TabulatorField.js
+++ client/TabulatorField.js
@@ -1,20 +1,24 @@
 (() => {
     // Private methods
 
+    const iconPlus =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24"><line x1="4" y1="12" x2="20" y2="12" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round"/><line y1="4" x1="12" y2="20" x2="12" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round"/></svg>';
+    const iconMinus =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24"><line x1="4" y1="12" x2="20" y2="12" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round"/></svg>';
     const iconTick =
-        '<svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 13L9 17L19 7" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="1.5"><path d="M5 13L9 17L19 7" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg>';
     const iconCross =
-        '<svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="1.5"><path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg>';
     const iconFirst =
-        '<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 0 24 24"><path d="M18.41 16.59L13.82 12l4.59-4.59L17 6l-6 6 6 6zM6 6h2v12H6z" fill="currentColor"/></svg>';
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18.41 16.59L13.82 12l4.59-4.59L17 6l-6 6 6 6zM6 6h2v12H6z" fill="currentColor"/></svg>';
     const iconLast =
-        '<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 0 24 24"><path d="M5.59 7.41L10.18 12l-4.59 4.59L7 18l6-6-6-6zM16 6h2v12h-2z" fill="currentColor"/></svg>';
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5.59 7.41L10.18 12l-4.59 4.59L7 18l6-6-6-6zM16 6h2v12h-2z" fill="currentColor"/></svg>';
     const iconNext =
-        '<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" fill="currentColor"/></svg>';
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" fill="currentColor"/></svg>';
     const iconPrev =
-        '<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" fill="currentColor"/></svg>';
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" fill="currentColor"/></svg>';
     const loader =
-        '<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 0 24 24" xml:space="preserve"><circle fill="currentColor" cx="4" cy="12" r="3"><animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin=".1"/></circle><circle fill="currentColor" cx="12" cy="12" r="3"><animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin=".2"/></circle><circle fill="currentColor" cx="20" cy="12" r="3"><animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin=".3"/></circle></svg>';
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" xml:space="preserve"><circle fill="currentColor" cx="4" cy="12" r="3"><animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin=".1"/></circle><circle fill="currentColor" cx="12" cy="12" r="3"><animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin=".2"/></circle><circle fill="currentColor" cx="20" cy="12" r="3"><animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin=".3"/></circle></svg>';
 
     /**
      * @param {number} n
@@ -66,10 +70,13 @@
     }
 
     /**
-     * @param {string} handler
+     * @param {string|Function} handler
      * @returns {Function}
      */
     function getGlobalHandler(handler) {
+        if (typeof handler === "function") {
+            return handler;
+        }
         if (handler.indexOf(".") !== -1) {
             var parts = handler.split(".");
             namespace = window[parts[0]];
@@ -314,7 +321,7 @@
         var v = cell.getValue();
         var formatted = "";
         if (v || formatterParams["notNull"]) {
-            formatted = window[formatterParams["function"]](v);
+            formatted = getGlobalHandler(formatterParams["function"])(v);
         }
         return formatted;
     };
@@ -539,14 +546,24 @@
                 ? "#" + editor.getAttribute("id")
                 : editor;
             var opts = editorParams.options || {};
-            var inst = window[editorParams.function](el, opts);
+            var inst = getGlobalHandler(editorParams.function)(el, opts);
+
+            if (editorParams.initCallback) {
+                getGlobalHandler(editorParams.initCallback)(editor, inst);
+            }
         });
 
         //when the value has been set, trigger the cell to update
         function successFunc() {
             editor.value = editor.value.trim();
+
+            // Prevent success if value hasn't changed
             if (editor.value == editor.dataset.prevValue) {
                 cancel();
+
+                if (editorParams.cancelCallback) {
+                    getGlobalHandler(editorParams.cancelCallback)(editor);
+                }
                 return;
             }
             if (editor.value || editorParams.notNull) {
@@ -554,6 +571,10 @@
                 editor.value = fmt.output;
             }
             success(editor.value);
+
+            if (editorParams.successCallback) {
+                getGlobalHandler(editorParams.successCallback)(editor, value);
+            }
         }
 
         editor.addEventListener("keydown", function (e) {
